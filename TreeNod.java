669306cc5efb2393ff7dd18373b69d38d819bf7a
Hakashin <115143196+HakashinTruth@users.noreply.github.com>
2022-10-11 09:39:18 +0100

    Nodes of the tree
    what each node on the tree contains including the root node

---
+++ TreeNod.java
@@ -0,0 +1,14 @@
+package binsrytreemain;
+
+public class TreeNode {
+    TreeNode left,right;
+    int value;
+
+  
+    public TreeNode(int value) {
+        left = null;
+        right = null;
+        this.value = value;
+    }
+    
+}
